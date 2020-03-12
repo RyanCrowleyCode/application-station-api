184fd0f078ead01b6ea0c646453b3d689117328a
Ryan Crowley <ryancrowleycode@gmail.com>
2020-03-12 15:14:34 -0500

    created event model and added to package

--- appstationapp/models/__init__.py
+++ appstationapp/models/__init__.py
@@ -2,3 +2,4 @@ from .company import Company
 from .status import Status
 from .job import Job
 from .application import Application
+from .event import Event
---
+++ appstationapp/models/event.py
@@ -0,0 +1,28 @@
+from django.db import models
+from .application import Application
+
+class Event(models.Model):
+    """
+    This class is responsible for creating the Event instances.
+
+    Author: 
+        Ryan Crowley
+    """
+
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    details = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+    class Meta:
+        ordering = ("start_time", ) 
+        verbose_name = ("event")
+        verbose_name_plural = ("events")
+
+    def __str__(self):
+        return 
+
+    
+
+
